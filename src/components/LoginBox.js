8fcd07172e28fea6d4dce9cc1c77499ca1aa3778
asahapde <60831459+asahapde@users.noreply.github.com>
2021-09-25 16:42:45 -0400

    added inputs

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -4,20 +4,31 @@ const LoginBox = () => {
     return (
         <div style={allStyle}>
             <h1>Login</h1>
+            <input style={inputStyle} placeholder='Email'/>
+            <input style={inputStyle} placeholder='Password'/>
+            <button>Login</button>
         </div>
     )
 }
 
 const allStyle = {
-    color:'white',
+    color: 'white',
     fontFamily: 'Racing Sans One',
     width: '30%',
     height: '500px',
     textAlign: 'center',
     margin: '5%',
     backgroundColor: '#6D597A',
-    borderRadius: '25px' ,
-    fontSize: '25px'
+    borderRadius: '25px',
+    fontSize: '25px',
+    display: 'flex',
+    flexDirection: 'column',
+}
+
+const inputStyle = {
+    marginLeft: '25%',
+    marginRight: '25%',
+    marginTop: '10%'
 }
 
 export default LoginBox
